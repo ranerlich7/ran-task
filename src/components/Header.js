33b9451cee27d798a3055dbe0205cfa1f85b961f
ran <ran@goz.com>
2022-12-17 08:06:33 +0200

    added container

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,7 @@ function Header() {
   return (
     <header className="header">
         <h1>Ran's Header</h1>
-        <Button name="Ran" color="red" onc="true"></Button>
-        <Button name="Add" color="blue" onc="false"></Button>
-        <Button name="Del" color="magenta" onc="false"></Button>
+        <Button name="Add" color="black" onc="true"></Button>
 
     </header>
   )
