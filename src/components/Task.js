cfb5ab5fb19a90be28e93619cf8eba55f69b6f8c
ran <ran@goz.com>
2022-12-17 08:06:49 +0200

    added single task

---
+++ src/components/Task.js
@@ -0,0 +1,14 @@
+
+function Task({content}) {
+  return (
+    <div >Task {content}</div>
+  )
+}
+
+export default Task
+
+// example of style in js
+// const taskStyle={
+//     color:'red',
+//     backgroundColor:'black'
+// }
