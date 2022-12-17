0216c8b4b982ffc9f48b753d1be63b898707704c
ran <ran@goz.com>
2022-12-17 07:54:47 +0200

    single task added properly

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,35 +1,33 @@
-import Button from "./Button"
+import Task from "./Task"
 
-let tasks = [
-    {
-        id: 1,
-        name:"Cleaning",
-        date:"Monday 9pm"
 
-    },
-    {
-        id: 2,
-        name:"Ironing",
-        date:"Sunday 9pm"
 
-    },
-    {
-        id: 3,
-        name:"Shopping",
-        date:"Saturday 9pm"
-
-    }
-]
-
-function getName(i){
-    return <li> {i.name} </li>
-}
 function Tasks() {    
-      return (
+    let tasks = [
+        {
+            id: 1,
+            name:"Cleaning",
+            date:"Monday 9pm"
+    
+        },
+        {
+            id: 2,
+            name:"Ironing",
+            date:"Sunday 9pm"
+    
+        },
+        {
+            id: 3,
+            name:"Shopping",
+            date:"Saturday 9pm"
+    
+        }
+    ]
+    return (
         //   for task in tasks
         //     f'<li>{task.name}</li>'
     <>    
-    {tasks.map((i) => <Task task={i}/>)}
+    {tasks.map((i) => <Task key={i.id} content={i.name}/>)}
     {/* {tasks.map((i) => <li>{i.name}</li>)}
     {tasks.map(getName)} */}
     </>
