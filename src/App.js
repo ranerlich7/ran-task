8f052cc84f64666980dec0534e291d74c4385b81
ran <ran@goz.com>
2022-12-22 11:13:34 +0200

    moved tasks to app.js and added useState hook for it

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import { useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0)
+  const [tasks, setTasks] = useState(
+  let tasks = [
+    {
+        id: 1,
+        name:"Cleaning",
+        date:"Monday 9pm"
+
+    },
+    {
+        id: 2,
+        name:"Ironing",
+        date:"Sunday 9pm"
+
+    },
+    {
+        id: 3,
+        name:"Shopping",
+        date:"Saturday 9pm"
+
+    }
+])
 
   function taskClick() {
     setCounter(counter + 1)
@@ -13,7 +34,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <Tasks counter={counter} taskClick={taskClick} />
+      <Tasks counter={counter} taskClick={taskClick} tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,26 +3,6 @@ import Task from "./Task"
 
 
 function Tasks({taskClick, counter}) {    
-    let tasks = [
-        {
-            id: 1,
-            name:"Cleaning",
-            date:"Monday 9pm"
-    
-        },
-        {
-            id: 2,
-            name:"Ironing",
-            date:"Sunday 9pm"
-    
-        },
-        {
-            id: 3,
-            name:"Shopping",
-            date:"Saturday 9pm"
-    
-        }
-    ]
     return (
     <>    
     {tasks.map((task) => <Task key={task.id} content={task.name} date={task.date} taskClick={taskClick} counter={counter}/>)}
