6661ec7c4609738d1590b98ae89ac27972420863
ran <ran@goz.com>
2022-12-22 11:19:58 +0200

    added button to delete all tasks + fix tasks

--- src/App.js
+++ src/App.js
@@ -4,37 +4,40 @@ import { useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0)
-  const [tasks, setTasks] = useState(
-  let tasks = [
-    {
+  const [tasks, setTasks] = useState([
+      {
         id: 1,
-        name:"Cleaning",
-        date:"Monday 9pm"
+        name: "Cleaning",
+        date: "Monday 9pm"
 
-    },
-    {
+      },
+      {
         id: 2,
-        name:"Ironing",
-        date:"Sunday 9pm"
+        name: "Ironing",
+        date: "Sunday 9pm"
 
-    },
-    {
+      },
+      {
         id: 3,
-        name:"Shopping",
-        date:"Saturday 9pm"
+        name: "Shopping",
+        date: "Saturday 9pm"
 
-    }
-])
+      }
+    ])
 
   function taskClick() {
     setCounter(counter + 1)
     console.log(counter)
   }
 
+  function deleteTask() {
+    setTasks([])
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks counter={counter} taskClick={taskClick} tasks={tasks}/>
+      <Tasks counter={counter} taskClick={taskClick} tasks={tasks} deleteTask={deleteTask} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,9 +2,10 @@ import Task from "./Task"
 
 
 
-function Tasks({taskClick, counter}) {    
+function Tasks({taskClick, counter, deleteTask, tasks}) {    
     return (
     <>    
+    <button onClick={deleteTask}>Del All</button>
     {tasks.map((task) => <Task key={task.id} content={task.name} date={task.date} taskClick={taskClick} counter={counter}/>)}
     </>
   )
