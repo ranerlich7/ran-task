c5935fa1445872244aa81f18e9359a0814735f51
Ran john brice <ranerlich@gmail.com>
2022-12-26 21:40:43 +0200

    added task reminder green stripe

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,10 +8,9 @@ import { FaTimes } from "react-icons/fa"
 
 function Task({ task, onDelete, toggleReminder }) {
     return (
-            <div className="task" onDoubleClick={()=>toggleReminder(task.id)}>                
+            <div className={task.reminder ? "task reminder": "task"} onDoubleClick={()=>toggleReminder(task.id)}>                
                 <h3>{task.name} <FaTimes onClick={()=>onDelete(task.id)} color="red" /></h3>
                 <p>{task.date}</p>
-                <p>{task.reminder ? 'REMIND': ''}</p>
             </div>
         )
     }
