1e579f98daba667e74c93168815ba86bc8640a8e
ran <ran@goz.com>
2023-01-02 11:59:42 +0200

    added delete task via api to json_server

--- src/App.js
+++ src/App.js
@@ -4,23 +4,20 @@ import { useState, useEffect } from 'react';
 import AddTask from './components/AddTask';
 
 function App() {
-  const [showAddTask,setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(false)
   useEffect(() => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks()
-      setTasks(tasksFromServer)
+    function getTasks(params) {
+      fetch('http://127.0.0.1:5000/tasks')
+        .then(response => response.json())
+        .then(json => {
+          setTasks(json)
+          console.log(json)
+        })
     }
-
     getTasks()
+
   }, [])
 
-  // Fetch Tasks
-  const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks')
-    const data = await res.json()
-
-    return data
-  }
 
   const [tasks, setTasks] = useState([])
 
@@ -30,27 +27,35 @@ function App() {
   // }
 
   function deleteTask(taskID) {
+    fetch('http://127.0.0.1:5000/tasks/'+taskID, {
+      method: 'DELETE',}).then(response => response.json())
+      .then(json => console.log(json))
+    
     setTasks(()=> tasks.filter((task)=>task.id === taskID ? null : task))
   }
 
-  function addTask(name, date, reminder){
-    setTasks([...tasks,{id:tasks.length + 1, name:name, date:date, reminder:reminder}])
+
+
+
+
+  function addTask(name, date, reminder) {
+    setTasks([...tasks, { id: tasks.length + 1, name: name, date: date, reminder: reminder }])
   }
 
   const toggleReminder = (id) => {
-    setTasks(tasks.map( task => task.id === id ? {...task, reminder:!task.reminder} : task))
+    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
   function onAdd() {
-      setShowAddTask(!showAddTask)
+    setShowAddTask(!showAddTask)
   }
- 
+
 
   return (
     <div className="container">
-      <Header onAdd={onAdd} showAddTask={showAddTask}/>
-      {showAddTask && <AddTask addTask={addTask}/>}
-      <Tasks tasks={tasks}  deleteTask={deleteTask} toggleReminder={toggleReminder}/>
+      <Header onAdd={onAdd} showAddTask={showAddTask} />
+      {showAddTask && <AddTask addTask={addTask} />}
+      <Tasks tasks={tasks} deleteTask={deleteTask} toggleReminder={toggleReminder} />
     </div>
   );
 }
