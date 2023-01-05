ea127bd3ed8a392cc71173f7ddc0bfac2c3af08b
ran <ran@goz.com>
2023-01-05 11:40:24 +0200

    toggle reminder now saving in db

--- src/App.js
+++ src/App.js
@@ -4,64 +4,80 @@ import { useEffect, useState } from 'react';
 import AddTask from './components/AddTask';
 
 function App() {
-    const [showAddForm, setShowAddForm] = useState(false)
-    const [tasks, setTasks] = useState([])
+  const [showAddForm, setShowAddForm] = useState(false)
+  const [tasks, setTasks] = useState([])
 
-    console.log('rendering component')
+  console.log('rendering component')
 
-    useEffect(() => {
-        // promise
-        // async await
-        fetch("http://localhost:5000/tasks")
-            .then((response) => response.json())
-            .then((data) => {
-                setTasks(data)
-            });
-        console.log('useEffect')
-    }, [tasks[0].reminder])
+  useEffect(() => {
+    // promise
+    // async await
+    fetch("http://localhost:5000/tasks")
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data)
+      });
+    console.log('useEffect')
+  }, [])
 
 
-    function onDelete(id) {
-        fetch(`http://localhost:5000/tasks/${id}` , { method: 'DELETE' })
+  function onDelete(id) {
+    fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE' })
 
-        setTasks(tasks.filter(task => task.id !== id))
-    }
+    setTasks(tasks.filter(task => task.id !== id))
+  }
 
-    function toggleReminder(id) {
-        setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
-    }
-
-    function addTask(name, date, reminder) {
-        let task = {
-            id: tasks.length + 10,
-            name: name,
-            date: date,
-            reminder: reminder
-
-        }
-        fetch('http://localhost:5000/tasks', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(task)
-        });
-
-        setTasks([...tasks, task])
+  function toggleReminder(task) {
+    let updatedTask = {
+      id: task.id,
+      name: task.name,
+      date: task.date,
+      reminder: !task.reminder
 
     }
+    fetch(`http://localhost:5000/tasks/${task.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    });
 
-    function toggleForm() {
-        setShowAddForm(!showAddForm)
+
+    setTasks(tasks.map(task2 => task2.id === task.id ? { ...task2, reminder: !task2.reminder } : task2))
+  }
+
+  function addTask(name, date, reminder) {
+    let task = {
+      id: tasks.length + 10,
+      name: name,
+      date: date,
+      reminder: reminder
+
     }
+    fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    });
 
-    return (
-        <div className="container">
-            <Header toggleForm={toggleForm} showAddForm={showAddForm} />
-            {showAddForm && <AddTask addTask={addTask} />}
-            <Tasks tasks={tasks} onDelete={onDelete} toggleReminder={toggleReminder} />
-        </div>
-    );
+    setTasks([...tasks, task])
+
+  }
+
+  function toggleForm() {
+    setShowAddForm(!showAddForm)
+  }
+
+  return (
+    <div className="container">
+      <Header toggleForm={toggleForm} showAddForm={showAddForm} />
+      {showAddForm && <AddTask addTask={addTask} />}
+      <Tasks tasks={tasks} onDelete={onDelete} toggleReminder={toggleReminder} />
+    </div>
+  );
 }
 
 export default App;
