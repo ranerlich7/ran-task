c2debef100b34bc94341d9c7fce756cf9af6fc63
ran <ran@goz.com>
2023-01-05 12:38:37 +0200

    useLocation hook to remove add button in about screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import Button from "./Button"
+import { useLocation } from 'react-router-dom'
 
-function Header({toggleForm, showAddForm}) {
+function Header({ toggleForm, showAddForm }) {
+  const location = useLocation()
   return (
     <header className="header">
-        <h1>Ran's Header</h1>
-        <Button name={showAddForm ?"Close" : "Add task"} color={showAddForm?"red":"black"} toggleForm={toggleForm}></Button>
-
+      <h1>Ran's Header</h1>
+      {location.pathname === '/' &&
+        <Button name={showAddForm ? "Close" : "Add task"} color={showAddForm ? "red" : "black"} toggleForm={toggleForm}></Button>
+      }
     </header>
   )
 }
